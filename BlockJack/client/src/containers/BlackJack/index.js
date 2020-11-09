3a6209a4fa309ab0018c59ec040bdf22032df498
ArturVargas <arturvargas9226@gmail.com>
2020-11-08 22:36:52 -0600

    import web3 and deploy SC

--- BlockJack/client/src/containers/BlackJack/index.js
+++ BlockJack/client/src/containers/BlackJack/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
 
-const Blackjack = () => (
-  <div>
-    Blackjack
-  </div>
-);
+import Blackjack from '../../contracts/BlackJack.json';
 
-export default Blackjack;
+const BlackjackHome = () => {
+  const [contract, setContract] = useState(undefined);
+  const [account, setAccount] = useState('');
+  const [balance, setBalance] = useState('');
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const networkId = await web3.eth.net.getId();
+      const accounts = await web3.eth.getAccounts();
+      const deployedNetwork = Blackjack.networks[networkId];
+      const contract = new web3.eth.Contract(
+        Blackjack.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+      await contract.methods.reset().call();
+      setContract(contract);
+      setAccount(accounts[0]);
+      if(account) {
+        const ethBalance = await web3.eth.getBalance(account);
+        setBalance(ethBalance);
+      }
+    }
+    init();
+    window.ethereum.on('accountsChanged', accounts => {
+      setAccount(accounts[0])
+    });
+  }, [account]);
+
+  const playerTurn = async () => {
+    console.log('-->> ', contract);
+    await contract.methods.playerTurn().call()
+    .then(console.log);
+  }
+
+  return (
+    <div>
+      {account}
+      {balance}
+      Blackjack
+      <button onClick={ () => playerTurn()}>Pedir Carta</button>
+    </div>
+  );
+};
+
+export default BlackjackHome;
--- BlockJack/client/src/containers/Home/index.js
+++ BlockJack/client/src/containers/Home/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.scss';
 
 const Home = () => (
-  <div>
-    Home
+  <div className='home-page'>
+    <div className='header'>
+      Hola <span> account </span> bienvenido a Crypto-Casino.
+    </div>
+    <div className='games'>
+      <div>
+        <span> Blackjack: </span>
+        <Link to='/blackjack'>
+          <button>Ir al juego</button>
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
