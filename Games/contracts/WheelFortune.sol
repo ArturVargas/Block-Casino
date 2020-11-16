22600a99523c4d9069e90ff82b4ad1edb9435fc2
ArturVargas <arturvargas9226@gmail.com>
2020-11-15 22:33:07 -0600

    add wheelFortune smart contract

---
+++ Games/contracts/WheelFortune.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.2;
+
+contract WheelFortune {
+
+}
+
+// rules.
+/* 
+ obtener el addres de player.
+ usar una funcion para que genere un numero aleatorio de vueltas que dara la ruleta.
+ usar stages para aceptar, cerrar y pagar apuestas.
+ recibir el monto de la apuesta del jugador para multiplicarlo por el numero que salga en la ruleta.
+ validar que la wallet del usuario cuente con el monto a apostar.
+ enviar la transaccion al ganador (player o sc) segun el caso. 
+*/
