ee51fce267d7e6779494d3980a6c1ddb42d4dad0
ArturVargas <arturvargas9226@gmail.com>
2020-12-06 22:45:55 -0600

    add first functions to smart contract

--- Games/contracts/WheelFortune.sol
+++ Games/contracts/WheelFortune.sol
@@ -2,7 +2,37 @@
 pragma solidity ^0.7.2;
 
 contract WheelFortune {
+  enum Stages {
+    MakeBet,
+    PayBet,
+    Finish
+  }
 
+  address public player;
+  uint public bet;
+  uint public prize;
+
+    function startPlay(uint _bet) public payable {
+      player = msg.sender;
+      bet = _bet;
+      require(_bet <= msg.value, "Fondos insuficientes");
+      prize = randomNumber();
+      winner();
+    }
+
+    function randomNumber() internal returns(uint) {
+      return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % 20;
+    }
+    
+    function winner() public payable {
+        uint result = (bet * prize);
+        if(result > 0) {
+            msg.sender.transfer(result);
+        }
+        if(result < 0) {
+            address(this).call{ value: bet }("");
+        }
+    }
 }
 
 // rules.
